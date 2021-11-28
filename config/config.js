fb07eb47142fffc86094c7e1ec317a5af49eab74
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2021-11-28 10:00:04 +0530

    added secret and mongo uri

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const config = {
+    production: {
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    },
+    development: {
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    }
+}
+
+
+exports.get = function get(env) {
+    return config[env] || config.development
+}
